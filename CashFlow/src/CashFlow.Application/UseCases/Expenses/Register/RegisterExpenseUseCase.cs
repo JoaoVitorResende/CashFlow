a2d05f5d69c6eb4570109ba0e0dde02071933ac4
joao vitor resende <54758120+JoaoVitorResende@users.noreply.github.com>
2024-10-17 20:31:23 -0300

    Utilização de validator do nuget

--- CashFlow/src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ CashFlow/src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -1,5 +1,4 @@
-﻿using CashFlow.Communication.Enuns;
-using CashFlow.Communication.Requests;
+﻿using CashFlow.Communication.Requests;
 
 namespace CashFlow.Application.UseCases.Expenses.Register;
 public class RegisterExpenseUseCase
@@ -7,22 +6,15 @@ public class RegisterExpenseUseCase
     public RequestRegisterExpenseJson Execute(RequestRegisterExpenseJson request)
     {
         Validate(request);
-        //TO DO VALIDATIONS
         return new RequestRegisterExpenseJson();
     }
     private void Validate(RequestRegisterExpenseJson request)
     {
-        var titleIsEmpty = string.IsNullOrWhiteSpace(request.Title);
-        var dateResult = DateTime.Compare(request.Date, DateTime.UtcNow);
-        var paymentTypeIsValid = Enum.IsDefined(typeof(PaymentType), request.PaymentType);
-
-        if (titleIsEmpty)
-            throw new ArgumentException("The title is required");
-        if(request.Amount <= 0)
-            throw new ArgumentException("The amount is necessary and above zero");
-        if(dateResult > 0)
-            throw new ArgumentException("The date must be in the present or in the past");
-        if (!paymentTypeIsValid)
-            throw new ArgumentException("The payment type must be one of the options");
+        var validator = new RegisterExpenseValidator();
+        var result = validator.Validate(request);
+        if (!result.IsValid)
+        {
+            var errosMessages = result.Errors.Select(f => f.ErrorMessage).ToList();
+        }
     }
 }

c04d516a84d8fbcb94b9305ce00d099a93f63f31
joao vitor resende <54758120+JoaoVitorResende@users.noreply.github.com>
2024-10-20 19:10:29 -0300

    finalização de customização de mensagens de erro

--- CashFlow/src/CashFlow.Api/Middleware/CultureMiddleware.cs
+++ CashFlow/src/CashFlow.Api/Middleware/CultureMiddleware.cs
@@ -8,10 +8,13 @@ public class CultureMiddleware
     public CultureMiddleware(RequestDelegate next) => _next = next;
     public async Task Invoke(HttpContext context)
     {
-        var culture = context.Request.Headers.AcceptLanguage.FirstOrDefault();
+        var supportedLanguages = CultureInfo.GetCultures(CultureTypes.AllCultures).ToList();
+        var requestedCulture = context.Request.Headers.AcceptLanguage.FirstOrDefault();
         var cultureInfo = new CultureInfo("en");
-        if(!string.IsNullOrWhiteSpace(culture))
-            cultureInfo = new CultureInfo(culture);
+        if(!string.IsNullOrWhiteSpace(requestedCulture) && supportedLanguages.Exists(language => language.Name.Equals(requestedCulture)))
+        {
+            cultureInfo = new CultureInfo(requestedCulture);
+        }
         CultureInfo.CurrentCulture = cultureInfo;
         CultureInfo.CurrentUICulture = cultureInfo;
         await _next(context);

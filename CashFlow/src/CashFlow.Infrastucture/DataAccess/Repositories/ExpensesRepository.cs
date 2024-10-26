82c048f127ba2ad68a08ab71fba11497647ce56b
joao vitor resende <54758120+JoaoVitorResende@users.noreply.github.com>
2024-10-26 13:11:10 -0300

    Refatoramento de criação de banco de dados aproveitando o injection
    Classe dependecyInjection de domain alterada para comportar injection na criação de banco de dados e expenses repository alterado para comportar mudanças na injection

--- CashFlow/src/CashFlow.Infrastucture/DataAccess/Repositories/ExpensesRepository.cs
+++ CashFlow/src/CashFlow.Infrastucture/DataAccess/Repositories/ExpensesRepository.cs
@@ -4,10 +4,14 @@ using CashFlow.Domain.Repositories.Expenses;
 namespace CashFlow.Infrastucture.DataAccess.Repositories;
 internal class ExpensesRepository : IExpensesRepository
 {
+    private readonly CashFlowDbContex _dbContext;
+    public ExpensesRepository(CashFlowDbContex dbContext)
+    {
+        _dbContext = dbContext;
+    }
     public void Add(Expense expense)
     {
-        var dbContext = new CashFlowDbContex();
-        dbContext.Add(expense);
-        dbContext.SaveChanges();
+        _dbContext.Add(expense);
+        _dbContext.SaveChanges();
     }
 }
